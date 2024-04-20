de57d7e5089cc2549ec9e9df607a14aea15ba068
HlaingHtun <hlainghtun2018@gmail.com>
2024-04-20 17:41:04 +0630

    update

--- src/main/java/com/sspd/Exericise/Main.java
+++
@@ -1,7 +0,0 @@
-package com.sspd.Exericise;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
---
+++ src/main/java/com/sspd/Loop/Ex_1.java
@@ -0,0 +1,4 @@
+package com.sspd.Loop;
+
+public class Ex_1 {
+}
