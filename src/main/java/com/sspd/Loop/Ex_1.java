435f84d905b66a7ee3cd9900648cb7ba9be26777
HlaingHtun <hlainghtun2018@gmail.com>
2024-04-20 17:43:07 +0630

    update

--- src/main/java/com/sspd/Loop/Ex_1.java
+++ src/main/java/com/sspd/Loop/Ex_1.java
@@ -1,4 +1,14 @@
 package com.sspd.Loop;
 
 public class Ex_1 {
+
+    public static void main(String[]arg){
+
+        for(int i = 0;i<=5;i++){
+
+            System.out.println(i);
+        }
+
+
+    }
 }
