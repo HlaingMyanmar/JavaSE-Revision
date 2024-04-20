fff8d31499a4863b4e3019f82fe610e5c39c8daa
HlaingHtun <hlainghtun2018@gmail.com>
2024-04-20 18:00:27 +0630

    update

---
+++ src/main/java/com/sspd/dowhileLoop/Ex_1.java
@@ -0,0 +1,32 @@
+package com.sspd.dowhileLoop;
+
+import javax.swing.*;
+
+public class Ex_1 {
+
+    public static void main(String[] args) {
+
+        int pass = 99;
+
+        int result;
+        String name ;
+
+
+        do {
+
+            name = JOptionPane.showInputDialog("Insert Your Name?");
+
+            result = Integer.parseInt(JOptionPane.showInputDialog("Insert Your Password"));
+
+            if(!(result == pass && name.equals("Hlaing"))){
+
+                JOptionPane.showMessageDialog(null, "Try Again?");
+
+            }
+
+        } while (result != pass || !(name.equals("Hlaing")));
+
+        JOptionPane.showMessageDialog(null, "Correct");
+
+    }
+}
