14eaabc1fc6fe1ca357d304446cf086d193150f2
HlaingHtun <hlainghtun2018@gmail.com>
2024-04-22 17:35:12 +0630

    update

---
+++ src/main/java/com/sspd/Method/Ex_1.java
@@ -0,0 +1,26 @@
+package com.sspd.Method;
+
+public class Ex_1 {
+
+    public static void main(String[] args) {  // Main method
+
+        getInfo();  // static to static
+
+        new Ex_1().getPrint();  // static to non-static
+
+
+    }
+
+    static void  getInfo(){
+
+
+        System.out.println("We are Developer");
+    }
+
+    void getPrint(){
+
+        System.out.println("I am Hlaing Htun");
+    }
+
+
+}
