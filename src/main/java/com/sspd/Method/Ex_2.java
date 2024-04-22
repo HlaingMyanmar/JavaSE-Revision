eee608beff5ac1e627d67335abaa2610b70f275d
HlaingHtun <hlainghtun2018@gmail.com>
2024-04-22 17:47:43 +0630

    update

--- src/main/java/com/sspd/Method/Ex_2.java
+++ src/main/java/com/sspd/Method/Ex_2.java
@@ -8,6 +8,9 @@ public class Ex_2 {
 
         System.out.println(getSum(4,5));
 
+        System.out.println(getSum(4.2,12.2));
+        System.out.println(getSum("Hlaing","Htun"));
+
     }
 
     int getSum(){
@@ -17,9 +20,20 @@ public class Ex_2 {
         return x;
     }
 
+    // Method Overloading -> Parameter
+
     static int getSum(int x, int y){// with Parameter
 
         return  x+y;
 
+    }
+    static double getSum(double x, double y){
+
+        return  x+y;
+    }
+
+    static String getSum(String st1, String st2){
+
+        return st1+st2;
     }
 }
