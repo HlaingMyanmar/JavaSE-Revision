419d981b9ecbf23be7fca5287e6bdfbd92498386
Dream Of Future <hlainghtun2018@gmail.com>
2024-04-22 15:29:58 +0630

    update

--- src/main/java/com/sspd/ifstatement/Ex_2.java
+++ src/main/java/com/sspd/ifstatement/Ex_2.java
@@ -1,6 +1,23 @@
 package com.sspd.ifstatement;
 
+import javax.swing.*;
+
 public class Ex_2 {
 
+    public static void main(String[] args) {
 
+        // Ternary operator  //Short Hand If...Else
+
+        //variable = (condition) ? expressionTrue :  expressionFalse;
+
+        String name = JOptionPane.showInputDialog("Enter Your Name ?");
+        String pass = JOptionPane.showInputDialog("Enter Your Password ?");
+
+        String result  = (name.equals("Hlaing") && pass.equals("99"))?"Correct":"Try Again";
+
+        JOptionPane.showMessageDialog(null,result);
+
+
+
+    }
 }
