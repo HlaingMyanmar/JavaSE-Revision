9044a8a7ae626e74be6eaa3afcc43f166db27898
SSPD Service And IT <hlainghtun2018@gmail.com>
2024-06-20 14:16:39 +0630

    update

---
+++ src/main/java/com/sspd/Inhertiance_1_Hierarchical/A.java
@@ -0,0 +1,19 @@
+package com.sspd.Inhertiance_1_Hierarchical;
+
+public class A {
+
+    public void print_A() { System.out.println("Class A"); }
+}
+
+class B extends A {
+    public void print_B() { System.out.println("Class B"); }
+}
+
+class C extends A {
+    public void print_C() { System.out.println("Class C"); }
+}
+
+class D extends A {
+    public void print_D() { System.out.println("Class D"); }
+}
+
