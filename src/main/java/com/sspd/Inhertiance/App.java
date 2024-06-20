01019ea8e1efb6eeec24f6e3467117aef6b5b4ac
SSPD Service And IT <hlainghtun2018@gmail.com>
2024-06-20 14:03:38 +0630

    update

---
+++ src/main/java/com/sspd/Inhertiance/App.java
@@ -0,0 +1,22 @@
+package com.sspd.Inhertiance;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        MountainBike mb = new MountainBike(3,4,20);
+
+
+        System.out.println("After Default ...........");
+        System.out.println(mb); // to String default
+
+        System.out.println("After Chaging..........");
+
+        mb.speedUp(20);
+        mb.applyBrake(1);
+
+        System.out.println(mb);
+
+
+    }
+}
---
+++ src/main/java/com/sspd/Inhertiance/MountainBike.java
@@ -0,0 +1,23 @@
+package com.sspd.Inhertiance;
+
+public class MountainBike extends Bicycle {
+
+
+    public int seatHeight;
+
+    MountainBike (int seatHeight,int gear,int speed){
+
+        super(gear,speed);
+        this.seatHeight = seatHeight;
+
+
+    }
+
+    @Override
+    public String toString() {
+
+        return "Speed : "+super.getSpeed()+"\nGear : "+super.getGear() + "\nSeatHeight : "+this.seatHeight;
+
+
+    }
+}
